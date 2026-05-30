1d9e2f4aa8aa750f87b7eb8657c3836c1fefc3c7
arifxlab <arifxlab@gmail.com>
2026-05-31 02:11:29 +0500

    Add health endpoint and Bruno API testing setup"

---
+++ src/main/java/com/neuralops/core/controller/HealthController.java
@@ -0,0 +1,13 @@
+package com.neuralops.core.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/")
+    public String health() {
+        return "NeuralOps Backend Running";
+    }
+}
