906c72c5a3829a476ec78d92da5178fe9a61c8de
arifxlab <arifxlab@gmail.com>
2026-05-31 01:56:14 +0500

    Initial Spring Boot project setup

---
+++ src/main/java/com/neuralops/core/NeuralopsCoreApplication.java
@@ -0,0 +1,13 @@
+package com.neuralops.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeuralopsCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NeuralopsCoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/neuralops/core/NeuralopsCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neuralops.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeuralopsCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
