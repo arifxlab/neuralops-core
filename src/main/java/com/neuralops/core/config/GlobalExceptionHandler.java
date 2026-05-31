edcbc28cd14bf83b3cf6e5b3b998fc1516a18628
arifxlab <arifxlab@gmail.com>
2026-05-31 14:10:04 +0500

    Working on backend

--- src/main/java/com/neuralops/core/config/GlobalExceptionHandler.java
+++ src/main/java/com/neuralops/core/config/GlobalExceptionHandler.java
@@ -1,4 +1,25 @@
 package com.neuralops.core.config;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<Map<String, Object>> handleRuntimeException(RuntimeException ex) {
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("message", ex.getMessage());
+        response.put("status", HttpStatus.BAD_REQUEST.value());
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(response);
+    }
 }
